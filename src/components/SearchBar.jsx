9ab568577262d4697548fe6a6b4a838c3b799f9a
Geoffrey You <107085735+geoffreynyc@users.noreply.github.com>
2023-05-06 18:21:15 -0400

    Initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./SearchBar.css";
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(term);
+  };
+  const handleChange = (e) => {
+    setTerm(e.target.value);
+  };
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleFormSubmit}>
+        <label>Search for images:</label>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
